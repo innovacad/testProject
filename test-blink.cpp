3f9b84478325ee8629f93cb9987bbdebc1ac5be2
fmedj <fmedj22@gmail.com>
2022-08-20 11:41:58 +0100

    added test-blink f

---
+++ test-blink.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string.h>
+void Print(string,input);
+int main() {
+    //std::cout << "je suis un developer(ou nom)";
+    Print("je suis un developer(ou nom)")
+    return 0;
+}
+void Print(string,input){
+
+    std::cout << stw(45);
+    std::cout<<input;
+    return 0;
+    
+}
+
