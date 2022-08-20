285ecde557b98065bb9af65c0bf7c8919d1e5d3b
taki <ah.kenioua@gmail.com>
2022-08-20 11:44:19 +0100

    added function

---
+++ print_tk.cpp
@@ -0,0 +1,24 @@
+#include <iostream>    
+#include <iomanip> 
+#include <string.h>     
+
+void Print (std::string input);
+
+int main()  {
+
+      std::cout <<"hello world!";
+      Print("I'm a developer");
+
+    return 0;
+}
+
+
+void Print(std::string input) {
+ /* takes string 'input' as argument and prints it 
+ on the center of the terminal*/
+    std::cout <<std::setw(43);
+    std::cout <<input;
+
+    
+  
+}
