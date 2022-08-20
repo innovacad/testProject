6af7f637d93e13121209ba75d6b1747900f5b708
iyad <iyadkdb@gmail.com>
2022-08-20 09:25:25 -0700

    new work blink

---
+++ blink_ik.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <windows.h>
+#include <iomanip> 
+
+using namespace std;
+
+
+
+void move(){
+    while(1){
+        system("CLS");
+        Sleep(3000);
+        Print();
+        Sleep(3000);
+    }
+}
